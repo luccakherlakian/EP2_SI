bf5ba683b7446a5ecf27dab148403ca0a8f96100
lucca <luccakherlakian@gmail.com>
2025-11-01 15:41:47 -0300

    Versão 2: Implementa CRUD funcional com Django Forms

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.all().order_by('-data_postagem')
@@ -11,29 +12,27 @@ def post_detail(request, pk):
 
 def post_create(request):
     if request.method == 'POST':
-        novo_titulo = request.POST.get('titulo')
-        novo_conteudo = request.POST.get('conteudo')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
 
-        Post.objects.create(
-            titulo=novo_titulo,
-            conteudo=novo_conteudo
-        )
-
-        return redirect('post_list')
-
-    return render(request, 'post_form.html')
+    return render(request, 'post_form.html', {'form': form})
 
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
-
     if request.method == 'POST':
-        post.titulo = request.POST.get('titulo')
-        post.conteudo = request.POST.get('conteudo')
-        post.save()
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
 
-        return redirect('post_detail', pk=post.pk)
-
-    return render(request, 'post_form.html', {'post': post})
+    return render(request, 'post_form.html', {'form': form})
 
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
