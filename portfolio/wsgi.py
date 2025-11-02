ea8f3588f9a8fca2e327d8a63bf2227c39d0b32a
lucca <luccakherlakian@gmail.com>
2025-11-02 13:02:02 -0300

    Configura projeto para produção (Gunicorn, WhiteNoise, Render)

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
